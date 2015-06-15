b78cebe7500600022c426a9d02882f0b515751d1
Diego Lafuente <dlaf88@gmail.com>
2015-06-15 14:29:46 +0000

    added validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,6 @@
 class Profile < ActiveRecord::Base
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :email_address, presence: true
+  validates :telephone, presence: true
 end
